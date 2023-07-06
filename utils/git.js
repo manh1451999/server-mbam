fa93e41ce31625ab44dea9d87c3d683d2c181adb
Đào Sỹ Mạnh <manhds@ftech.ai>
2023-07-06 16:13:26 +0700

    update

--- utils/git.js
+++ utils/git.js
@@ -1,6 +1,7 @@
 const cmd = require("node-cmd");
+const { stringToBoolean } = require(".");
 module.exports.autoPushProxyGit = ()=>{
-  if(process.env.AUTO_PUSH_GIT=='false') return;
+  if(!stringToBoolean(process.env.AUTO_PUSH_GIT)) return;
   console.log('node-cmd')
     cmd.run('chmod 777 autoPushGit.sh'); /* :/ Fix no perms after updating */
     cmd.run('sh autoPushGit.sh', (err, data) => {  // Run our script
@@ -11,7 +12,7 @@ module.exports.autoPushProxyGit = ()=>{
 }
 
 module.exports.autoPullProxyGit = ()=>{
-  if(process.env.AUTO_PUSH_GIT=='false') return;
+  if(!stringToBoolean(process.env.AUTO_PUSH_GIT)) return;
   console.log('node-cmd')
     cmd.run('chmod 777 autoPullGit.sh'); /* :/ Fix no perms after updating */
     cmd.run('sh autoPullGit.sh', (err, data) => {  // Run our script
